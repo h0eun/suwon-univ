d0a82333f0aca4c56c22b2e216911d2129315d83
instructor <instructor@example.com>
2022-01-10 12:55:57 +0900

    add files

---
+++ workspaces/java-basic/examples-a/src/Ex01HelloJava.java
@@ -0,0 +1,18 @@
+// 라인 주석 : // 표시부터 그 줄의 끝까지 주석
+/* 영역 주석 : 시작과 끝을 사용해서 주석 표시 */
+
+public class Ex01HelloJava {
+
+	public static void main(String[] args) {	// 프로그램이 시작되는 곳 : entry point
+		
+		// system.out.println("TEST"); // 오류 : 대소문자 구분
+		System.out.println("Hello, Java Programming World !!!!!"); // ; --> 실행문의 끝 ( 필수 )
+		System.out.println("First Java Program");
+		System.out.println("Bye ~~~");
+
+	} // 프로그램 종료
+	
+	// "문자열" : "를 사용해서 문자열 표시
+	// '문자' : '를 사용해서 한 개의 문자 표시
+
+}
