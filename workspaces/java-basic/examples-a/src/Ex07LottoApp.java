9030344105f461b9cf6241300fa2c8379e3c8405
instructor <instructor@example.com>
2022-01-11 13:30:14 +0900

    update code

--- workspaces/java-basic/examples-a/src/Ex07LottoApp.java
+++ workspaces/java-basic/examples-a/src/Ex07LottoApp.java
@@ -6,22 +6,68 @@ public class Ex07LottoApp {
 	public static void main(String[] args) {		
 		System.out.println("로또 당첨 예상 번호 뽑기를 시작합니다.");
 		System.out.println();
-		
+				
 		outer : while (true) {	
 			String selection = showMenu();
 			System.out.println();
-			
+		
+			int[] numbers = null;
 			switch (selection) {
-			case "1": break;
+			case "1": 
+				do {
+					numbers = selectBasicNumbers();
+				} while (/* 평균 검사 */ !checkMean(numbers) ); // !true -> false, !false -> true
+				// 출력
+				showNumbers(numbers);
+				break;
 			case "9": 
-				System.out.println("행운을 빕니다.");
-				System.out.println("프로그램을 종료합니다.");
+				System.out.println("*** 행운을 빕니다.");
+				System.out.println("*** 프로그램을 종료합니다.");
 				break outer;
+			default : 
+				System.out.println("*** 지원하지 않는 명령입니다.");
 			}
 			
 			System.out.println();			
 		}		
 		scanner.close();
+	}
+
+	private static boolean checkMean(int[] numbers) {
+		int sum = 0;
+		for (int number : numbers) {
+			sum += number;
+		}
+		int mean = sum / numbers.length;
+//		if (mean >= 20 && mean <= 26) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+		return ( mean >= 20 && mean <= 26 );
+	}
+
+	private static void showNumbers(int[] numbers) {
+		System.out.print("SELECTED NUMBERS : ");
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.printf("[%2d]", numbers[i]);
+		}
+		System.out.println();
+	}
+
+	private static int[] selectBasicNumbers() {
+		int[] numbers;
+		// 기본 규칙이 적용된 번호 뽑기
+		numbers = new int[6];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = (int)(Math.random() * 45) + 1; // 1 ~ 46 (46은 포함하지 않음)
+			for (int j = 0; j < i; j++) {
+				if (numbers[i] == numbers[j]) {
+					i--; // i = -1;
+				}
+			}
+		}
+		return numbers;
 	}
 	
 	public static String showMenu() {
